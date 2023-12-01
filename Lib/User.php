e77cf0ffc2f1d52bec1e1583c3c9e3acd9492cfe
Ivan <ivansos7890@gmail.com>
2023-12-01 22:50:19 +0200

    New class User

--- Lib/User.php
+++ Lib/User.php
@@ -2,7 +2,55 @@
 
 namespace Lib;
 
+use Exception;
+
+
+
 class User
 {
+    private string $name;
+    private int $age;
 
+    private string $email;
+
+    public function __construct(string $name, int $age, string $email)
+    {
+        $this->setName($name);
+        $this->setAge($age);
+        $this->email = $email;
+    }
+
+    public function __call($args, $methods)
+    {
+            if (!method_exists($this, $methods))
+            {
+                echo "Цей метод '{$methods}' не існує /n";
+            }
+            call_user_func([$this, $methods], $args);
+    }
+
+    public function getAll(): void
+    {
+       echo $this->name . PHP_EOL. $this->age . PHP_EOL . $this->email;
+    }
+
+
+    private  function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    private function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+}
+
+$user = new User('Денис', 27, 'gmail.com');
+
+try{
+    $user->getAll();
+}catch(Exception $e)
+{
+    dd($e);
 }
